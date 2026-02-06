00f7c8d836695e231b96205695829997ceca90f3
Annisya Rianta <annisyarianta@gmail.com>
2026-02-06 10:49:45 +0700

    update backend

--- backend-karyawan/src/app.module.ts
+++ backend-karyawan/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [EmployeesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService],
 })
 export class AppModule {}
